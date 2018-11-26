de5bb356d66adcd0a0bc04d91dee8245d8497092
Yurao <yuridf@gmail.com>
2018-11-26 17:27:09 -0200

    Listagem de funcionarios

--- src/app/listagem/listagem.component.ts
+++ src/app/listagem/listagem.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-listagem',
@@ -7,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListagemComponent implements OnInit {
 
-  constructor() { }
+  url: string = '../../assets/img/leao.jpg';
+  lotacao: string = 'CrossFit';
+  nome: string = 'João da Silva';
+
+  listaFuncionario: object[] = [
+                                  // {
+                                  //   url: 'url1',
+                                  //   lotacao: 'lotacao1',
+                                  //   nome: 'nome1'
+                                  // },
+                                  // {
+                                  //   url: 'url2',
+                                  //   lotacao: 'lotacao2',
+                                  //   nome: 'nome2'
+                                  // }
+                               ];
+
+  constructor(http: HttpClient) {
+    http.get<object[]>('http://localhost:8080/pontointeligenteapi/funcionario').subscribe(
+      funcionarios => this.listaFuncionario = funcionarios,
+      erro => console.log(erro));
+  }
 
   ngOnInit() {
   }
